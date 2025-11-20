e66a2ea3b153a708fdab95f56a343a6261959759
OKIMOTO Yusuke <you_oki1@yahoo.co.jp>
2025-11-21 00:00:07 +0900

    トークナイザを実装

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,141 @@
-use std::io::{stdout, Write};
+use once_cell::sync::Lazy;
+use regex::Regex;
+use std::io;
 
-fn tokenize(s: &str) -> Vec<String> {
-    let spreaded = s.replace("(", " ( ").replace(")", " ) ");
-    let tokens: Vec<String> = spreaded
-        .split_whitespace()
-        .map(|item| item.to_string())
-        .collect();
+static RE_NUMBER: Lazy<Regex> = Lazy::new(|| Regex::new(r"^[0-9]+$").unwrap());
+static RE_WORD: Lazy<Regex> = Lazy::new(|| Regex::new(r"^[a-zA-Z\+\*/\-]+$").unwrap());
 
-    return tokens;
+use std::fmt;
+
+trait Expression: fmt::Debug {}
+
+trait Atom {
+    type Val;
+
+    fn val(&self) -> &Self::Val;
 }
 
-fn read_from(tokens: Vec<String>) -> Vec<String> {
-    if tokens.len() == 0 {
-        println!("unexpected EOF while reading");
-    }
-
-    return tokens;
+#[derive(Debug)]
+struct IntAtom {
+    n: i32,
 }
 
-fn eval(split: std::vec::Vec<std::string::String>) -> i32 {
-    let tokens = read_from(split);
+impl Expression for IntAtom {}
 
-    let op = &tokens[1];
-    let a: i32 = tokens[2].parse().unwrap();
-    let b: i32 = tokens[3].parse().unwrap();
+impl Atom for IntAtom {
+    type Val = i32;
 
-    if op == "+" {
-        return a + b;
-    } else if op == "-" {
-        return a - b;
-    } else if op == "*" {
-        return a * b;
-    } else if op == "/" {
-        return a / b;
-    } else {
-        panic!("unexpected operator"); // FIXME エラー処理を実施
+    fn val(&self) -> &Self::Val {
+        &self.n
     }
 }
+
+#[derive(Debug)]
+struct SymbolAtom {
+    name: String,
+}
+
+impl Expression for SymbolAtom {}
+
+impl Atom for SymbolAtom {
+    type Val = String;
+
+    fn val(&self) -> &Self::Val {
+        &self.name
+    }
+}
+
+// TODO Boxとdynの意味を理解する
+#[derive(Debug)]
+struct ListExpression {
+    elems: Vec<Box<dyn Expression>>,
+}
+
+impl Expression for ListExpression {}
 
 fn main() {
-    loop {
-        print!("lis.rs> ");
-        std::io::stdout().flush().unwrap();
+    let mut input_str: String = String::new();
 
-        let mut exp = String::new();
-        std::io::stdin().read_line(&mut exp).ok();
+    println!("input lisp expressoin:");
+    io::stdin()
+        .read_line(&mut input_str)
+        .expect("Failed to read line");
 
-        let val = eval(tokenize(&exp));
-        println!("{}", val);
-    }
+    let tokens: Box<dyn Expression> = parse(&input_str);
+
+    // FIXME debug
+    println!("{tokens:?}");
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn parse(s: &str) -> Box<dyn Expression> {
+    let result = s.replace("(", "( ").replace(")", " )");
+    let trimmed_str = result.trim();
+    let tokens: Vec<String> = trimmed_str
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect();
+    parse_rec(tokens[0..tokens.len()].to_vec())
+}
 
-    #[test]
-    fn test_tokenize() {
-        let input = "(+ 1 2)";
+fn parse_rec(tokens: Vec<String>) -> Box<dyn Expression> {
+    if tokens.len() == 1 {
+        let token_str = &tokens[0];
+        return match token_str.as_str() {
+            s if RE_NUMBER.is_match(s) => Box::new(IntAtom {
+                n: s.parse().unwrap(),
+            }),
+            s if RE_WORD.is_match(s) => Box::new(SymbolAtom {
+                name: s.to_string(),
+            }),
+            _ => panic!("Unknown type token: {}", token_str),
+        };
+    }
 
-        let expected = vec!["(", "+", "1", "2", ")"];
-        let actual = tokenize(input);
+    // リスト
+    // TODO: 本来はほかの特殊形式も返せるようにする
+    let mut ls = ListExpression { elems: Vec::new() };
+    assert!(tokens.get(0).unwrap() == "(");
 
-        for (e, a) in expected.iter().zip(actual.iter()) {
-            assert_eq!(e, a);
+    let mut idx = 0;
+    loop {
+        idx += 1;
+        if idx == tokens.len() {
+            break;
+        }
+
+        let token: String = tokens[idx].clone();
+
+        match token.as_str() {
+            "(" => {
+                let mut end_j = 0;
+                for j in (idx + 1)..(tokens.len()) {
+                    if tokens[j] == ")" {
+                        end_j = j;
+                        break;
+                    }
+                }
+
+                if end_j == 0 {
+                    panic!("Ummatched open parenthesis");
+                }
+
+                let inner_list_s = &tokens[idx..(end_j + 1)];
+                let inner_list = parse_rec(inner_list_s.to_vec());
+                ls.elems.push(inner_list);
+                idx = end_j;
+                continue;
+            }
+            ")" => {
+                if idx == (tokens.len() - 1) {
+                    break;
+                } else {
+                    panic!("Unmatched close parenthesis");
+                }
+            }
+            _ => ls.elems.push(parse_rec(vec![token])),
         }
     }
+    // TODO かっこが対応していなかったらエラー出す
 
-    #[test]
-    fn test_eval1() {
-       let input = vec!["(", "+", "2", "3", ")"].into_iter().map(|s| s.to_string()).collect();
-
-       let expected = 5;
-       let actual = eval(input);
-
-       assert_eq!(expected, actual);
-    }
-
-    #[test]
-    fn test_eval2() {
-       let input = vec!["(", "-", "2", "3", ")"].into_iter().map(|s| s.to_string()).collect();
-
-       let expected = -1;
-       let actual = eval(input);
-
-       assert_eq!(expected, actual);
-    }
-
-    #[test]
-    fn test_eval_nested_parrents() {
-       let input = vec!["(", "+", "2", "(", "+", "3", "4", ")", ")"].into_iter().map(|s| s.to_string()).collect();
-
-       let expected = 9;
-       let actual = eval(input);
-
-       assert_eq!(expected, actual);
-    }
+    Box::new(ls)
 }
